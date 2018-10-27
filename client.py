e93803d446d49f6795433bfb84bb3f5ec0732935
AlejandroEsLo <alex-lojim@hotmail.com>
2018-10-27 10:44:36 +0200

    Ejercicio 7 acabado

--- client.py
+++ client.py
@@ -7,17 +7,20 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-IP = sys.argv[1]
-PORT = int(sys.argv[2])
-METODO = sys.argv[3]
-DIRECCION = sys.argv[4]
+try:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    METODO = sys.argv[3]
+    DIRECCION = sys.argv[4]
+    EXPIRES = sys.argv[5]
+except IndexError:
+    print("Usage: client.py ip puerto register sip_address expires_value")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    Mensaje = (METODO.upper() + " sip:" + DIRECCION + " SIP/2.0\\r\\n") 
+    my_socket.connect((IP, PORT))
+    Mensaje = (METODO.upper() + " sip:" + DIRECCION + " SIP/2.0\\r\\n\n" \
+                + "EXPIRES: "  + EXPIRES + "\\\r\\n\r\\n") 
     print('Enviando: ', Mensaje)
     my_socket.send(bytes(Mensaje, 'utf-8') + b'\\r\\n')
     data = my_socket.recv(1024)
--- server.py
+++ server.py
@@ -24,6 +24,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         metodo = mensaje_cliente[0]
         direccion = mensaje_cliente[1].split(":")
         usuario = direccion[1]
+        expires = int(mensaje_cliente[3].split("\\")[0])
         
         self.wfile.write(b"SIP/2.0 200 OK\\r\\n\\r\\n")
         
@@ -31,15 +32,27 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         #Imprimimos Ip y Puerto del cliente
 #        print ("IP: {}".format(Ip) + "  Puerto: {}".format(puerto)+ \
 #                    " Metodo: {}".format(metodo) +" Direccion: {}".format(direccion))
-
+        
         if metodo == "REGISTER":
             self.list_serv = {}
+            usuarios_expires =[]
             self.list_serv["IP"] = Ip
             self.list_serv["DIRECCION"] = usuario
+            self.list_serv["EXPIRES"] = expires            
             list_clients[usuario] = self.list_serv
-      
-        print ("Lista clientes: {}".format(list_clients))
-        
+            
+            #Eliminamos cliente si a expires = 0
+            if expires == 0:
+                print( usuario , "EXPIRADO")
+                usuarios_expires.append(usuario)
+                    
+                for usuario in usuarios_expires:
+                    del list_clients[usuario]
+            else:
+                print( usuario , "REGISTRADO")
+            
+        print ("Lista clientes: {}\n".format(list_clients))
+        print ("Lista ELIMINADOS: {}".format(usuarios_expires))
 if __name__ == "__main__":
     
     list_clients ={}
