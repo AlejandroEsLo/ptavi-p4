6fafe7c41869c3b0add85849bb28e1bde7592232
AlejandroEsLo <alex-lojim@hotmail.com>
2018-10-28 14:02:04 +0100

    Falta json ejercicio8

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
@@ -22,12 +23,15 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         Ip = self.client_address[0]
         puerto = self.client_address[1]
         metodo = mensaje_cliente[0]
-        direccion = mensaje_cliente[1].split(":")
-        usuario = direccion[1]
+        direccion = mensaje_cliente[1]
+        usuario = direccion.split(":")[1]
         expires = int(mensaje_cliente[3].split("\\")[0])
         
-        self.wfile.write(b"SIP/2.0 200 OK\\r\\n\\r\\n")
-        
+        t_inicio = time.strftime("%Y-%m-%d %H:%M:%S", 
+                                 time.gmtime(int(time.time())))
+        t_expires = time.strftime("%Y-%m-%d %H:%M:%S",
+                                  time.gmtime(int(time.time() + expires)))
+
 #        print("El cliente nos manda ", ' '.join(mensaje_cliente))
         #Imprimimos Ip y Puerto del cliente
 #        print ("IP: {}".format(Ip) + "  Puerto: {}".format(puerto)+ \
@@ -38,21 +42,25 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             usuarios_expires =[]
             self.list_serv["IP"] = Ip
             self.list_serv["DIRECCION"] = usuario
-            self.list_serv["EXPIRES"] = expires            
-            list_clients[usuario] = self.list_serv
+            self.list_serv["EXPIRES"] = t_expires            
+            list_clients[direccion] = self.list_serv
             
-            #Eliminamos cliente si a expires = 0
-            if expires == 0:
-                print( usuario , "EXPIRADO")
-                usuarios_expires.append(usuario)
-                    
-                for usuario in usuarios_expires:
-                    del list_clients[usuario]
-            else:
-                print( usuario , "REGISTRADO")
+            # Eliminamos cliente si expires = 0 
+            # O si su tiempo es menor que la hora actual
+            for clients in list_clients:
+                inf = list_clients[clients]
+                if inf["EXPIRES"] <= t_inicio:
+                     usuarios_expires.append(clients)
+                            
+            for usuario in usuarios_expires:
+                print(usuario , "EXPIRADO\n")
+                del list_clients[usuario]
+
+        self.wfile.write(b"SIP/2.0 200 OK\\r\\n\\r\\n")
             
-        print ("Lista clientes: {}\n".format(list_clients))
-        print ("Lista ELIMINADOS: {}".format(usuarios_expires))
+        print ("Lista clientes: {}".format(list_clients))
+        print ("Lista ELIMINADOS: {}\n".format(usuarios_expires))
+
 if __name__ == "__main__":
     
     list_clients ={}
