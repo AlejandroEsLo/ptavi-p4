ab41efc3ca101d22db3c997c4c1b4225f0608347
Alejandro Estaban Lopez <aesteban@beta01.aulas.gsyc.urjc.es>
2018-10-30 14:00:44 +0100

    Cliente y Servidor acabado y pasado PEP8 y PEP257

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor.
-"""
+"""Programa cliente UDP que abre un socket a un servidor."""
 
 import socket
 import sys
@@ -19,8 +17,8 @@ except IndexError:
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((IP, PORT))
-    Mensaje = (METODO.upper() + " sip:" + DIRECCION + " SIP/2.0\\r\\n\n" \
-                + "EXPIRES: "  + EXPIRES + "\\r\\n") 
+    Mensaje = (METODO.upper() + " sip:" + DIRECCION + " SIP/2.0\\r\\n\n" +
+               "EXPIRES: " + EXPIRES + "\\r\\n")
     print('Enviando: ', Mensaje)
     my_socket.send(bytes(Mensaje, 'utf-8') + b'\\r\\n')
     data = my_socket.recv(1024)
